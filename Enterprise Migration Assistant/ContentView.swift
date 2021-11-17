2acfacd2cd19749f8b112eac9012d9d751506774
Cybertunnel <tylermorgan210@gmail.com>
2021-11-17 16:10:37 -0500

    Initial Commit

---
+++ Enterprise Migration Assistant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Enterprise Migration Assistant
+//
+//  Created by Morgan, Tyler on 11/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Enterprise Migration Assistant/Enterprise_Migration_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enterprise_Migration_AssistantApp.swift
+//  Enterprise Migration Assistant
+//
+//  Created by Morgan, Tyler on 11/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Enterprise_Migration_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
